b35fd30a0604882794d15617dd8d4c0f413e5b20
magician <magzhank05@gmail.com>
2021-05-12 00:59:11 +0600

    Max/min values for price and ratings

--- petshop1/apps/petshop/models.py
+++ petshop1/apps/petshop/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class product(models.Model):
     name = models.CharField('name of product', max_length = 50)
     pic_url = models.CharField('url for picture of product', max_length = 200)
     description = models.CharField('description of product', max_length = 200)
-    rating = models.IntegerField('rating', default=0)
-    price = models.IntegerField('price', default=0)
+    rating = models.IntegerField('rating', default=0, validators = [MaxValueValidator(5), MinValueValidator(0)])
+    price = models.IntegerField('price', default=0, validators = [MinValueValidator(0)])
 
     class Meta:
         verbose_name_plural = "products"
