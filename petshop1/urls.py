e8644cc4fa68f88c0be51405fbce502e606e1543
magician <magzhank05@gmail.com>
2021-05-21 03:00:41 +0600

    error

--- petshop1/urls.py
+++ petshop1/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('petshop.urls')),
+    path('', include(('petshop.urls', 'petshop'), namespace='petshop')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
