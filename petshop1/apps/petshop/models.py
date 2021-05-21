cfd4be9815efde6ff8d565904b16c085191a074c
Balkiyaeva <80237484+Balkiyaeva@users.noreply.github.com>
2021-05-21 10:48:30 +0600

    обновила просто

--- petshop1/apps/petshop/models.py
+++ petshop1/apps/petshop/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
+
 class product(models.Model):
     name = models.CharField('name of product', max_length = 50)
     pic_url = models.CharField('url for picture of product', max_length = 200)
@@ -10,5 +11,6 @@ class product(models.Model):
 
     class Meta:
         verbose_name_plural = "products"
+
     def __str__(self):
         return self.name
--- petshop1/apps/petshop/urls.py
+++ petshop1/apps/petshop/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name = 'Main'),
-    path('product/<int:id>', views.to_product, name = 'buy'),
-    path('search/', views.search, name = 'search'),
-    path('register/', views.register, name = 'register'),
+    path('', views.index, name='Main'),
+    path('product/<int:id>', views.to_product, name='buy'),
+    path('search/', views.search, name='search'),
+    path('register/', views.register, name='register'),
     path('logout/', views.log_out, name='logout'),
     path('loggedout/', views.logged_out, name='logged_out'),
-    path('user_account_page/<int:id>', views.load_account_page, name = 'personal_page')
+    path('user_account_page/<int:id>', views.load_account_page, name='personal_page')
 ]
