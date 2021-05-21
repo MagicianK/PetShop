7be1849777537a2aed62ba09a58452c8f39255b0
magician <magzhank05@gmail.com>
2021-05-21 11:31:19 +0600

    adf

--- petshop1/urls.py
+++ petshop1/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('petshop.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    url(r'',include('petshop.urls',namespace="petshop"))
 ]
