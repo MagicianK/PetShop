d15ad02dbe39594c7745f6f3fc0cd7c770759e45
Balkiyaeva <80237484+Balkiyaeva@users.noreply.github.com>
2021-05-31 17:27:56 +0600

    login?x3 last try

--- petshop1/urls.py
+++ petshop1/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.conf.urls import url
 from django.urls import path, include
 
+from petshop1.apps import petshop
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('',include('petshop.urls'))
+    path('', include('petshop.urls', namespace='petshop')),
 ]
