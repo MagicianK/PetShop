dbee33f0bcd31b888adb91f9af49370c39d436c1
magician <magzhank05@gmail.com>
2021-05-31 18:38:47 +0600

    design лич каб

--- petshop1/apps/petshop/urls.py
+++ petshop1/apps/petshop/urls.py
@@ -3,12 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    #path('user_page/<int:id>', views.load_account_page, name='mypage'),
+    path('user_page/<int:id>', views.load_account_page, name='mypage'),
     path('', views.index, name='Main'),
     path('product/<int:id>', views.to_product, name='buy'),
     path('search/', views.search, name='search'),
     path('register/', views.register, name='register'),
     path('loggedout/', views.logged_out, name='logged_out'),
-    path('search/search_by_stats', views.search_by_stats, name='search_by_stats'),
-    path('account/', views.login_user_page, name='account'),
+    path('search/search_by_stats', views.search_by_stats, name='search_by_stats')
 ]
